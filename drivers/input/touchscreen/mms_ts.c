7b32bbbc9f653b4f4b9c0a75beb81cd75813ce04
andip71 <andreasp@gmx.de>
2013-01-10 12:27:10 +0100

    mms_ts: Add sys interface to allow switching of touch boost
    off or on (default)
    
    Change-Id: Ibdc2e0bd29473fe8032c4bf828b2cd1d99a53bcb

--- drivers/input/touchscreen/mms_ts.c
+++ drivers/input/touchscreen/mms_ts.c
@@ -56,6 +56,8 @@
 #include <linux/touch_wake.h>
 #endif
 
+#include "touchboost_switch.h"
+
 #define MAX_FINGERS		10
 #define MAX_WIDTH		30
 #define MAX_PRESSURE		255
@@ -504,8 +506,11 @@ static void release_all_fingers(struct mms_ts_info *info)
 	}
 	input_sync(info->input_dev);
 #if TOUCH_BOOSTER
-	set_dvfs_lock(info, 2);
-	pr_debug("[TSP] dvfs_lock free.\n ");
+	if (tb_switch == TOUCHBOOST_ON)
+	{
+		set_dvfs_lock(info, 2);
+		pr_info("[TSP] dvfs_lock free.\n ");
+	}
 #endif
 }
 
@@ -748,7 +753,10 @@ static irqreturn_t mms_ts_interrupt(int irq, void *dev_id)
 #endif
 
 #if TOUCH_BOOSTER
-	set_dvfs_lock(info, !!touch_is_pressed);
+	if (tb_switch == TOUCHBOOST_ON)
+	{
+		set_dvfs_lock(info, !!touch_is_pressed);
+	}
 #endif
 out:
 	return IRQ_HANDLED;
@@ -3123,12 +3131,15 @@ static int __devinit mms_ts_probe(struct i2c_client *client,
 	}
 
 #if TOUCH_BOOSTER
-	mutex_init(&info->dvfs_lock);
-	INIT_DELAYED_WORK(&info->work_dvfs_off, set_dvfs_off);
-	INIT_DELAYED_WORK(&info->work_dvfs_chg, change_dvfs_lock);
-	bus_dev = dev_get("exynos-busfreq");
-	info->cpufreq_level = -1;
-	info->dvfs_lock_status = false;
+	if (tb_switch == TOUCHBOOST_ON)
+	{
+		mutex_init(&info->dvfs_lock);
+		INIT_DELAYED_WORK(&info->work_dvfs_off, set_dvfs_off);
+		INIT_DELAYED_WORK(&info->work_dvfs_chg, change_dvfs_lock);
+		bus_dev = dev_get("exynos-busfreq");
+		info->cpufreq_level = -1;
+		info->dvfs_lock_status = false;
+	}
 #endif
 
 	i2c_set_clientdata(client, info);
